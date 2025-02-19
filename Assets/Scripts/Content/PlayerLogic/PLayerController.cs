f9d7c9b4630619893537364568810399a98f845c
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-02-19 02:50:21 -0800

    chore: удалена тестовая имплементация Text Popup

--- Assets/Scripts/Content/PlayerLogic/PLayerController.cs
+++ Assets/Scripts/Content/PlayerLogic/PLayerController.cs
@@ -19,8 +19,7 @@ namespace Assets.Scripts.Content.PlayerLogic
         private void Construct(
             CharacterJumpHandler jumpHandler, 
             InputSystemActions inputActions, 
-            Animator animator,
-            PopupTextController popupTextController)
+            Animator animator)
         {
             _inputActions = inputActions;
             _jumpHandler = jumpHandler;
@@ -29,8 +28,6 @@ namespace Assets.Scripts.Content.PlayerLogic
             _playerData.ThisEntity = this;
 
             _inputActions.Player.Jump.performed += OnInputJump;
-
-            _inputActions.Player.Attack.performed += context => popupTextController.ShowDamage(_playerData.JumpObjectTransform.position, 10);
         }
 
         private void OnInputJump(InputAction.CallbackContext context)
