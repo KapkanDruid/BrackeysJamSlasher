40c282335ff76a7d3c651a2dc5869662818bdc6d
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-02-20 16:59:54 -0800

    feat: реализован скрипт спавна противников по триггеру

---
+++ Assets/Scripts/Content/CombatController.cs
@@ -0,0 +1,165 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Content
+{
+    public class CombatController : MonoBehaviour
+    {
+        [SerializeField] private List<EntitySubList> _enemyWaves;
+        [SerializeField] private List<float> _wavesDelay;
+        [SerializeField] private List<Transform> _spawnPoints;
+        [SerializeField] private List<GameObject> _walls;
+
+        private EnemyDeadHandler[] _enemies;
+        private bool _isComplete;
+        private bool _combatStarted;
+
+        private void Start()
+        {
+            List<EnemyDeadHandler> enemies = new();
+
+            foreach (var wave in _enemyWaves)
+            {
+                foreach (var entity in wave.Entities)
+                {
+                    var enemy = entity.ProvideComponent<EnemyDeadHandler>();
+
+                    if (enemy != null)
+                        enemies.Add(enemy);
+                }
+            }
+
+            _isComplete = false;
+
+            foreach (var wall in _walls)
+                wall.SetActive(false);
+
+            _enemies = enemies.ToArray();
+        }
+
+        private void Update()
+        {
+            CheckForEndCondition();
+        }
+
+        private void CheckForEndCondition()
+        {
+            if (_isComplete)
+                return;
+
+            for (int i = 0; i < _enemies.Length; i++)
+            {
+                if (!_enemies[i].IsDead)
+                {
+                    return;
+                }
+            }
+
+            _isComplete = true;
+
+            EndCombat();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (_combatStarted)
+                return;
+
+            if (!collision.TryGetComponent(out IEntity entity))
+                return;
+
+            Flags flags = entity.ProvideComponent<Flags>();
+
+            if (flags == null)
+                return;
+
+            if (!flags.Contain(EntityFlags.Player))
+                return;
+
+            StartCombat();
+        }
+
+        private void StartCombat()
+        {
+            Debug.Log("Combat Started!");
+
+            foreach (var wall in _walls)
+                wall.SetActive(true);
+
+            SpawnEnemiesAsync().Forget();
+
+            _combatStarted = true;
+        }
+
+        private async UniTask SpawnEnemiesAsync()
+        {
+            if (_combatStarted)
+                return;
+
+            for (int i = 0; i < _enemyWaves.Count; i++)
+            {
+                IEntity[] entities = _enemyWaves[i].Entities.ToArray();
+
+                float waveDelay = (i < _wavesDelay.Count) ? _wavesDelay[i] : _wavesDelay[_wavesDelay.Count - 1];
+
+                for (int j = 0; j < entities.Length; j++)
+                {
+                    var enemyTransform = entities[j].ProvideComponent<Transform>();
+
+                    if (enemyTransform != null)
+                        SpawnEnemy(enemyTransform);
+                }
+
+                try
+                {
+                    await UniTask.WaitForSeconds(waveDelay, cancellationToken: this.GetCancellationTokenOnDestroy());
+                }
+                catch (OperationCanceledException)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void SpawnEnemy(Transform enemyTransform)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, _spawnPoints.Count - 1);
+
+            enemyTransform.position = _spawnPoints[randomIndex].position;
+
+            enemyTransform.gameObject.SetActive(true);
+        }
+
+        private void EndCombat()
+        {
+            foreach (var wall in _walls)
+                wall.SetActive(false);
+        }
+    }
+
+    [Serializable]
+    public class EntitySubList
+    {
+        [SerializeField] private List<GameObject> _entities;
+
+        public List<IEntity> Entities
+        {
+            get
+            {
+                List<IEntity> entities = new();
+
+                foreach (var gameObject in _entities)
+                {
+                    if (gameObject.TryGetComponent(out IEntity entity))
+                        entities.Add(entity);
+                    else
+                        Debug.LogError("Entities array must contain only IEntity objects!");
+                }
+
+                return entities;
+            }
+        }
+    }
+}
